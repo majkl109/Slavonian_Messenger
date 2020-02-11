3084c39f438869f63aa9ceb95ee94a3da9829eaf
Majkl <majkl.andracic@gmail.com>
2020-02-11 12:41:46 +0100

    Share message

--- app/src/main/java/com/example/slavonianmessenger/CreateMessageActivity.java
+++ app/src/main/java/com/example/slavonianmessenger/CreateMessageActivity.java
@@ -1,8 +1,11 @@
 package com.example.slavonianmessenger;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -12,6 +15,13 @@ public class CreateMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_message);
     }
     public void onSendMessage(View view){
-        
+        EditText messageView = findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,messageText);
+
+        startActivity(intent);
     }
+
 }
